627f88c8bfb94026d012114e44e0fce676ed6a97
Erbert Bryan Moralde <ebzmoralde@addu.edu.ph>
2021-10-24 11:34:51 +0800

    Initial commit

---
+++ myapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse('Hello World')
+
+def my_name(request):
+    context = {'name': 'Erbert Moralde'}
+    return render(request, 'myapp/myapp.html', context)
